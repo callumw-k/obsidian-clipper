a542b0cf41375301ec7e21c0afb58c28095aab2a
Callum Wellwood-Kane <business@callumwk.com>
2024-12-08 01:01:28 +1100

    removed service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,8 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+
     }
 }
