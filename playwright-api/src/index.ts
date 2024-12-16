4267ebf711affd1b65628fec70983914d925f35e
Callum Wellwood-Kane <business@callumwk.com>
2024-12-16 21:49:16 +1100

    changed the server

--- playwright-api/src/index.ts
+++ playwright-api/src/index.ts
@@ -5,9 +5,10 @@ import { chromium, devices } from 'playwright';
 const app = new Hono();
 const browser = await chromium.launch({
     proxy: {
-        server: 'http://gate.smartproxy.com:10001',
+        server: 'gate.smartproxy.com',
         password: 'PKclfcFv2w1~Xw40qk',
         username: 'spkkoto9n4',
+        port: 10001,
     },
 });
 
