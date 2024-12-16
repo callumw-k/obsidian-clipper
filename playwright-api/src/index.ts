c175076d92902647da348534d8dd68001ffdc436
Callum Wellwood-Kane <business@callumwk.com>
2024-12-16 18:16:18 +1100

    updated prod dockerfile

--- playwright-api/src/index.ts
+++ playwright-api/src/index.ts
@@ -1,41 +1,48 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
 import { chromium, devices } from 'playwright';
 
-const app = new Hono()
+const app = new Hono();
 const browser = await chromium.launch();
 const context = await browser.newContext(devices['iPhone 11']);
 
 app.post('/', async (c) => {
-  const body = await c.req.json();
-  if (!("url" in body)) return c.json({ error: "URL not found" });
-  const page = await context.newPage();
-  await page.goto(body.url);
-  let title = "";
-  let image = ""
-  try {
-    title = await page.title();
-    image = await page.locator('meta[property="og:image"]').getAttribute('content', { timeout: 1000 }) || "";
-  } catch (_) {
-    title = "Couldn't process title"
-  }
+    const body = await c.req.json();
+    if (!('url' in body)) return c.json({ error: 'URL not found' });
+    const page = await context.newPage();
+    await page.goto(body.url);
+    let title = '';
+    let image = '';
+    try {
+        title = await page.title();
+    } catch (_) {
+        console.error("Couldn't process title");
+    }
 
-  return c.json({ "title": title, imageUrl: image });
-})
+    try {
+        image =
+            (await page
+                .locator('meta[property="og:image"]')
+                .getAttribute('content', { timeout: 3000 })) || '';
+    } catch (e) {
+        console.error(e);
+    }
 
-const port = 3000
+    return c.json({ title: title, imageUrl: image });
+});
+
+const port = 3000;
 
 const handleShutdown = async (signal: string) => {
-  console.log(`Received ${signal}. Closing browser and shutting down.`);
-  await browser.close();
-  process.exit(0);
+    console.log(`Received ${signal}. Closing browser and shutting down.`);
+    await browser.close();
+    process.exit(0);
 };
 
 process.on('SIGINT', handleShutdown); // Handle Ctrl+C
 process.on('SIGTERM', handleShutdown);
 
 serve({
-  fetch: app.fetch,
-  port
-
-})
+    fetch: app.fetch,
+    port,
+});
